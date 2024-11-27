25c729c8c79b5c42af712956e3877808e91bae8f
Gregor <43413852+Albadylic@users.noreply.github.com>
2024-11-27 22:06:53 +0000

    Collect Gen 3 moves

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [pokemon, setPokemon] = useState("pikachu");
+  const [moves, setMoves] = useState([]);
+
+  const isMoveGenIII = (move: any) => {
+    return (
+      move.version_group_details.filter(
+        (move: any) => move["version_group"]["name"] === "emerald"
+      ).length > 0
+    );
+  };
+
+  const handleSubmit = async () => {
+    const formatPokemon = (str: string) => {
+      return str.toLowerCase();
+    };
+    const url = `https://pokeapi.co/api/v2/pokemon/${formatPokemon(pokemon)}/`;
+
+    const response = await fetch(url);
+
+    const json = await response.json();
+
+    const moves = json["moves"]
+      .filter(isMoveGenIII)
+      .map((move: any) => move.move);
+
+    setMoves(moves);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Gen III Moveset Checker</h1>
       </header>
+      <main>
+        <h3>Type a Pokemon</h3>
+        <label htmlFor="pokemon_input">Enter the name of a Pokemon</label>
+        <input type="text" name="pokemon" id="pokemon_input" />
+        <button onClick={handleSubmit}>Go!</button>
+
+        {moves.map((move: any) => (
+          <div>
+            <p>{move.name}</p>
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
